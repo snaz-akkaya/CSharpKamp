c0a4759415e71043713b3586f766f7eda0c9aa17
snaz-akkaya <akkayanazsude@gmail.com>
2024-02-16 16:58:09 +0300

    Entities folder is created in KodlamaIO project. Category, Course, and Instructor classes is created in Entities folder.

---
+++ KodlamaIO/KodlamaIO/Entities/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodlamaIO.Entities;
+
+public class Course
+{
+    public required int Id { get; set; }
+    public required int InstructorId { get; set; }
+    public required string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdatedDate { get; set; }
+}
+
+
